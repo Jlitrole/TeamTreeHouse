2dac8f18aebb4e8e748279b1aefe209bbfca7638
Joe Laurle <Josue.laurole@gmail.com>
2021-01-06 20:02:55 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+alert("Hello, world!");
+console.log("Hello from the console.");
+alert("Thanks for visisting");
+document.write("<h1>Welcome to my web page.</h1.");
+
+
+
+alert("Warning! This message will self-destruct in");
+
+alert("3...")
+alert("2...")
+alert("1...")
